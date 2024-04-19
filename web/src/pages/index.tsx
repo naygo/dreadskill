3f199184a45c9bef2704edf67377a76e9e6aa0dd
Nayla Gomes <nayla.cgs@gmail.com>
2024-04-18 21:41:18 -0300

    feat: add second section

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,20 @@
 import Head from "next/head";
+import Image from "next/image";
+
+import clsx from "clsx";
 
 import { Navbar } from "@/components/Navbar";
-import Image from "next/image";
+
+import LogoWithLetters from "@/public/images/dreadskill-logo-letters.png";
 import Person1 from "@/public/images/person1.jpg";
 import Person2 from "@/public/images/person2.jpg";
 import Person3 from "@/public/images/person3.jpg";
 import Person4 from "@/public/images/person4.jpg";
-import clsx from "clsx";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
+
+const sectionStyle = "px-4 md:py-20";
+const socialButton =
+  "cursor-pointer text-4xl bg-dsOrange hover:bg-dsOrangeDark p-1.5 rounded-full fill-white";
 
 export default function Home() {
   return (
@@ -31,11 +39,68 @@ export default function Home() {
         <ImageCard src={Person4} />
       </section>
 
-      <main className="container">
-        <section id="sobre">
-          <h1>Conheça nosso trabalho</h1>
+      <main className="container mx-auto flex flex-col gap-10">
+        <section
+          id="sobre"
+          className={clsx(sectionStyle, "flex flex-col gap-24")}
+        >
+          <div className="flex flex-col md:flex-row gap-5 md:gap-10">
+            <div>
+              <Image
+                src={LogoWithLetters}
+                alt="DreadSkill"
+                className="hidden md:block lg:w-12/12 xl:w-11/12"
+              />
+            </div>
+
+            <div className="flex flex-col gap-4 md:gap-5">
+              <h1>Conheça nosso trabalho</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                id eros fringilla, sodales ipsum et, posuere felis. Quisque elit
+                felis, tincidunt a ex mattis, ultricies cursus lectus. Donec
+                ipsum lorem, pretium et consequat id, auctor in arcu. Morbi arcu
+                nunc, imperdiet in elit a, luctus aliquet risus. Duis quis
+                tellus ex. Ut pulvinar ipsum non elementum cursus.
+              </p>
+
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                id eros fringilla, sodales ipsum et, posuere felis. Quisque elit
+                felis, tincidunt a ex mattis, ultricies cursus lectus. Donec
+                ipsum lorem, pretium et consequat id, auctor in arcu. Morbi arcu
+                nunc, imperdiet in elit a, luctus aliquet risus. Duis quis
+                tellus ex. Ut pulvinar ipsum non elementum. Qualquer dúvida,
+                entre em contato conosco pelo WhatsApp ou pelo Instagram.
+              </p>
+
+              <div className="flex gap-4">
+                <Image
+                  src={LogoWithLetters}
+                  alt="DreadSkill"
+                  className="w-5/12 md:hidden"
+                />
+                <div className="flex flex-col gap-4">
+                  <p className="text-xs md:text-base">
+                    Nos siga no Instagram, acompanhe nosso trabalho e fique por
+                    dentro das novidades!
+                  </p>
+                  <div className="flex gap-2">
+                    <FaInstagram className={socialButton} />
+                    <FaWhatsapp className={socialButton} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="servicos-oferecidos" className={clsx(sectionStyle, "")}>
+          <h1>Serviços oferecidos</h1>
         </section>
       </main>
+
+      <footer className="h-24 p-10 text-center">copyright dreadskill c</footer>
     </>
   );
 }
