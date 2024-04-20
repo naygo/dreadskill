b4ed33c3e1119f30a5ff77f3f27da45484f53f06
Nayla Gomes <nayla.cgs@gmail.com>
2024-04-19 22:34:13 -0300

    feat: add button component

---
+++ web/src/components/Button.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+import { ButtonHTMLAttributes, DetailedHTMLProps, forwardRef } from "react";
+
+type NativeProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
+
+type ExtraProps = {
+  label: string;
+};
+type Props = NativeProps & ExtraProps;
+
+function ButtonGenerate(
+  { color, disabled, label, ...props }: Props,
+  ref: React.ForwardedRef<HTMLButtonElement>
+) {
+  return (
+    <button
+      ref={ref}
+      disabled={disabled}
+      {...props}
+      className={clsx(
+        "bg-dsOrange hover:bg-dsOrangeDark py-2 px-4 rounded text-white",
+        props.className
+      )}
+    >
+      {label}
+    </button>
+  );
+}
+
+const Button = forwardRef<HTMLButtonElement, Props>(ButtonGenerate);
+export default Button;
